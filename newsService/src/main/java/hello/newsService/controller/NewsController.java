811b36dcca4edb5895dafd5935e8650c1687e40e
Lee Seungwoo <oltmddn123@naver.com>
2024-05-23 00:28:52 +0900

    뉴스삭제 및 뉴스상세  구현
    controller 매핑

--- newsService/src/main/java/hello/newsService/controller/NewsController.java
+++ newsService/src/main/java/hello/newsService/controller/NewsController.java
@@ -5,9 +5,7 @@ import hello.newsService.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,27 @@ public class NewsController {
         model.addAttribute("newslist", news);
         return "basic/newsList";
     }
+
+    @PostMapping("/")
+    public String 뉴스목록2(Model model) {
+
+        List<News> news = newsService.getAllNews();
+        model.addAttribute("newslist", news);
+        return "basic/newsList";
+    }
+    @GetMapping("/{newsId}")
+    public String 뉴스상세(@PathVariable int newsId, Model model) {
+        News newsById = newsService.getNewsById(newsId);
+        model.addAttribute("news", newsById);
+        return "basic/newsView";
+    }
+
+    @GetMapping("/del")
+    @ResponseBody
+    public String 뉴스삭제() {
+
+        return "ok";
+    }
+
 
 }
